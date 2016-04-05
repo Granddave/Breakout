0b4250b8ec1faa416949d9b493cd3853f5d6f313
David Isaksson <davidisaksson93@gmail.com>
2016-04-05 20:51:29 +0200

    Snyggade till

--- Breakout/breakout.cpp
+++ Breakout/breakout.cpp
@@ -11,6 +11,7 @@ Breakout::Breakout(QWidget *parent)
 	qDebug() << "HasMouseTracking:" << hasMouseTracking();
 	rack = new Racket();
 	boll = new Boll();
+
 	spelplan = new QRect(0, 0, W_WIDTH, W_HEIGHT);
 
 
@@ -32,8 +33,6 @@ Breakout::Breakout(QWidget *parent)
 			}
 		}
 	}
-
-
 
 	timer = new QTimer(this);
 	connect(timer, SIGNAL(timeout()), this, SLOT(update()));
@@ -63,7 +62,6 @@ void Breakout::paintEvent(QPaintEvent * e)
 void Breakout::mouseMoveEvent(QMouseEvent* e)
 {
 	rack->setPosition(e->x());
-	//qDebug() << e->x();
 }
 
 void Breakout::mousePressEvent(QMouseEvent* e)
@@ -77,8 +75,6 @@ void Breakout::update() //hitcheck
 
 	for (int i = 0; i < _blocks.size(); i++)
 		_blocks[i]->hitCheck(*boll);
-
-	//
 
 	repaint();
 }
