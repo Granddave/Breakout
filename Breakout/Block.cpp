cb760251d95c61067453b3add237bcf0ab00b563
alexemanuelol <alexemanuelol@hotmail.com>
2016-04-06 21:07:53 +0200

    block bild tillagd

--- Breakout/Block.cpp
+++ Breakout/Block.cpp
@@ -31,18 +31,21 @@ Block::Block(int x, int y)
 void Block::paint(QPainter& painter) const
 {
 	QPixmap pix("Resources/background.png");
+	QPixmap block("block.png");
 	if (times == 0)
 		painter.drawPixmap(0, 0, pix);
+
+	painter.drawPixmap(_x, _y, block);
 	times++;
-	if (_isActive)
-	{
-		//Ritar ut hitboxar
-		painter.setBrush(Qt::red);
-		painter.drawPolygon(*HBbottom);
-		painter.drawPolygon(*HBleft);
-		painter.drawPolygon(*HBright);
-		painter.drawPolygon(*HBtop);
-	}
+	//if (_isActive)
+	//{
+	//	//Ritar ut hitboxar
+	//	painter.setBrush(Qt::red);
+	//	painter.drawPolygon(*HBbottom);
+	//	painter.drawPolygon(*HBleft);
+	//	painter.drawPolygon(*HBright);
+	//	painter.drawPolygon(*HBtop);
+	//}
 }
 
 void Block::hitCheck(Boll& boll)
